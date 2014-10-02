44cf57f0dab5fcafa1e7c16739e1a814d783a6e0
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-10-01 22:36:27 -0400

    Add generalized Upset and UpsertBy functions for models

--- models/base.go
+++ models/base.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"fmt"
+	"strings"
+
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
 
@@ -11,7 +14,54 @@ type Model struct {
 	ID bson.ObjectId `bson:"_id,omitempty"`
 }
 
+func Upsert(m interface{}, DB *mgo.Database) error {
+	return UpsertBy(m, "ID", DB)
+}
+
+func UpsertBy(model interface{}, fieldStr string, DB *mgo.Database) error {
+	// _, err := models.Collection(new(Room), DB).Upsert(bson.M{"": r.ServiceID}, m)
+	// return err
+
+	typ := reflect.TypeOf(model)
+	val := reflect.ValueOf(model)
+
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+		val = val.Elem()
+	}
+
+	field, found := typ.FieldByName(fieldStr)
+
+	if !found {
+		return fmt.Errorf("Field '%s' not found", fieldStr)
+	}
+
+	dbName := strings.Split(field.Tag.Get("bson"), ",")[0]
+
+	// Default it to the lowercase field name if it wasn't provide by the
+	// BSON tag
+	if dbName == "" {
+		dbName = strings.ToLower(field.Name)
+	}
+
+	b := bson.M{dbName: val.FieldByName(fieldStr)}
+	_, err := Collection(reflect.New(typ).Interface(), DB).Upsert(b, model)
+
+	return err
+}
+
 func Collection(model interface{}, DB *mgo.Database) *mgo.Collection {
-	modelName := reflect.TypeOf(model).Elem().Name()
+	typ := reflect.TypeOf(model)
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+
+	path := strings.Split(typ.PkgPath(), "models/")
+	modelPkg := ""
+	if len(path) > 1 {
+		modelPkg = strings.Replace(path[1], "/", "_", -1) + "_"
+	}
+
+	modelName := strings.ToLower(modelPkg) + strings.ToLower(typ.Name())
 	return DB.C(modelName)
 }
